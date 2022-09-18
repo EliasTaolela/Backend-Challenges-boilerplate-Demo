d9e39a53b512c686f2c1a2e62a272e8357fa614f
Elias Taolela <37247904+EliasTaolela@users.noreply.github.com>
2022-09-18 18:51:23 +0200

    Second save

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ if (!process.env.DISABLE_XORIGIN) {
 }
 
 app.use('/public', express.static(process.cwd() + '/public'));
-
 app.route('/_api/package.json')
   .get(function(req, res, next) {
     console.log('requested');
